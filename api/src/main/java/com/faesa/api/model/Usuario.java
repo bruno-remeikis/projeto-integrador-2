79f5bcfdc53927c0134fa3eff3931499b0f4922e
Bruno Remeikis <beunocoutinhoremeikis@gmail.com>
2023-11-02 17:15:24 -0300

    Apaga getters e setters

--- api/src/main/java/com/faesa/api/model/Usuario.java
+++ api/src/main/java/com/faesa/api/model/Usuario.java
@@ -9,8 +9,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Usuario
 {
 	private int id;
@@ -21,53 +19,4 @@ public class Usuario
 	private byte[] foto;
 	private byte[] fotoCapa;
 	private Date dtInsert;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public String getBio() {
-		return bio;
-	}
-	public void setBio(String bio) {
-		this.bio = bio;
-	}
-	public byte[] getFoto() {
-		return foto;
-	}
-	public void setFoto(byte[] foto) {
-		this.foto = foto;
-	}
-	public byte[] getFotoCapa() {
-		return fotoCapa;
-	}
-	public void setFotoCapa(byte[] fotoCapa) {
-		this.fotoCapa = fotoCapa;
-	}
-	public Date getDtInsert() {
-		return dtInsert;
-	}
-	public void setDtInsert(Date dtInsert) {
-		this.dtInsert = dtInsert;
-	}
 }
